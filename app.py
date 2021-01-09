1c5f3755d54241f63cb0612acf05be88eb8c609e
Moses A. Samuel <mosamuel98@gmail.com>
2021-01-08 20:10:57 -0800

    add more validation logic

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """
-A basic web app that asks user to rate their experience shopping with a fake Lexus dealership
+A basic web app that asks the user to rate their experience shopping with a fake Lexus dealership
 """
 
 from flask import Flask, render_template, request
@@ -30,11 +30,12 @@ class Feedback(db.Model):
     rating = db.Column(db.Integer)
     comments = db.Column(db.Text())
 
-    def __init__(self, customer, dealer, rating, comments):
-        self.customer = customer
-        self.dealer = dealer
-        self.rating = rating
-        self.comments = comments
+    # def __init__(self, customer, dealer, rating, comments):
+    def __init__(self, **kwargs):
+        self.customer = kwargs['customer']
+        self.dealer = kwargs['dealer']
+        self.rating = kwargs['rating']
+        self.comments = kwargs['comments']
 
 @app.route('/')
 def index():
@@ -47,9 +48,19 @@ def submit():
         dealer = request.form['dealer']
         rating = request.form['rating']
         comments = request.form['comments']
+
         if customer == '' or dealer == '':
             return render_template('index.html', message='Please enter required fields')
-        return render_template('success.html')
+
+        # Save the user's feedback to the db unless they've already submitted a survey. In that case, inform them that their feedback has already been submitted.
+        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
+            data = Feedback(customer=customer, dealer=dealer, rating=rating, comments=comments)
+            db.session.add(data)
+            db.session.commit()
+
+            return render_template('success.html')
+        else:
+            return render_template('index.html', message='You have already submitted feedback')
 
 if __name__ == "__main__":
     app.run()
